619421ae88bd362a36507ed7087cc773d20a35b0
JamesGageMiller <156767564+JamesGageMiller@users.noreply.github.com>
2025-04-12 22:11:10 -0500

    Added in basic Landing page, student and instructor views

--- .js/app.js
+++ .js/app.js
@@ -8,6 +8,23 @@ $('#btnSwapRegister').on('click',function(){
         $('#frmLogin').slideDown('slow')
     })
 })
+$('#btnSwapLandingToLogin').on('click',function(){
+    $('#frmLandingPage').slideUp('slow',function(){ 
+        $('#frmLogin').slideDown('fast')
+    }   )
+})
+$('#btnSwapLandingToRegister').on('click',function(){
+    $('#frmLandingPage').slideUp('slow',function(){ 
+        $('#frmRegister').slideDown('fast')
+    }   )
+})
+//reserving space ahead of time for the following
+//:transition from the login page to the dashboard, dashboard will feature
+// -- a student/teacher view
+// -- teacher view will have the ability to have classes, classes will split into groups that will be made up of clumps of student ids. teachers will be able to make some system to make random groups after a code is entered to be put into a group
+// studnet view will see any feedback requested. and fill out
+// teacher view can also view feedback results and share them anonmously with students. no ids will be shown, instead names
+// logic for these boards will not be fully fleshed out yet, as most of it will depend on stuff being returned from the backend and will depend on the format of the backend.
 
 document.querySelector("#btnLogin").addEventListener("click",(e) => {
     //alert("Test");
@@ -66,9 +83,9 @@ document.querySelector('#btnRegistration').addEventListener('click',(e)=>{
         strMessage += '<p  class="mb-0 mt-0">Username must be an email address</p>'
     }
 
-    if(strPassword.length < 1){
+    if(strPassword.length < 15 || strPassword.length >=64){
         blnError = true
-        strMessage += "<p class ='mb-0 mt-0'>Password cannot be blank</p>"
+        strMessage += "<p class ='mb-0 mt-0'>Password must be greater then 15 characters and less then 64</p>"
     }
 
     if(!regPhoneNumber.test(strPhoneNumber)){
